c36be09bc3de2e66ca14c5ed4f8afc8f2cfe1e60
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2025-07-15 23:06:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Permutation/next-permutation.java
@@ -0,0 +1,40 @@
+class Solution {
+    void nextPermutation(int[] arr) {
+        // code here
+        int pivot = -1;
+        int n = arr.length;
+        for(int i=n-2;i>=0;i--){
+         if(arr[i]<arr[i+1]){
+             pivot = i;
+             break;
+         }
+        }
+        
+        if(pivot == -1){
+            reverse(arr,0,n-1);
+            return;
+        }
+        
+        for(int i=n-1;i>pivot;i--){
+            if(arr[i]>arr[pivot]){
+                int temp = arr[i];
+                arr[i] = arr[pivot];
+                arr[pivot] = temp;
+                break;
+            }
+        }
+        reverse(arr,pivot+1,n-1);
+    }
+        
+     public static void reverse(int[] arr,int start , int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+  // void swap(int)
+}
