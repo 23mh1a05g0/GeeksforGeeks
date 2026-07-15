5d7738cd5017247ef62186944bf72d3c9728bc24
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-15 17:41:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally Traversing a Matrix/spirally-traversing-a-matrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    public ArrayList<Integer> spirallyTraverse(int[][] mat) {
+        // code here
+        ArrayList <Integer> result = new ArrayList<>();
+        
+        int top = 0;
+        int bottom = mat.length-1;
+        int left = 0;
+        int right = mat[0].length-1;
+        
+        while(top <= bottom && left <= right){
+            if(top <= bottom && left <= right){
+                for(int i=left;i<=right;i++){
+                    result.add(mat[top][i]);
+                }
+                top++;
+            }
+            if(top <= bottom && left <= right){
+                for(int i=top;i<=bottom;i++){
+                    result.add(mat[i][right]);
+                }
+                right--;
+            }
+            if(top <= bottom && left <= right){
+                for(int i=right;i>=left;i--){
+                    result.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            if(top <= bottom && left <= right){
+                for(int i=bottom;i>=top;i--){
+                    result.add(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
