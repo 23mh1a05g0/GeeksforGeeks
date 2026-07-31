6e47466af0f4caf079157d27c855a29b386d918b
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-31 22:17:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Bits/reverse-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        // code here
+        int result = 0;
+        while(n != 0){
+            result = (result << 1) | (n & 1);
+            n = n >>> 1;
+        }
+        return result;
+    }
+}
