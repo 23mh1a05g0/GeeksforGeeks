c43da0c6c889a2696c0a505d7475a5fba61ee0f1
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-08-18 18:31:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -1,9 +1,27 @@
 class Solution {
     int search(int[] arr, int key) {
         // code here
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] == key){
-                return i;
+        int left = 0;
+        int right = arr.length-1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            
+            if(arr[left] <= arr[mid]){
+                if(arr[left] <= key && key < arr[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(arr[mid] < key && key <= arr[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
             }
         }
         return -1;
